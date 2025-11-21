c69815c79dc6857398dbfd36c3bde385c243be63
Epée Epée Landry <landryepee5@gamil.com>
2025-11-21 19:51:20 +0100

    Enhancement of the precedent crate and addition of the classifier metrics

--- src/core/validation.rs
+++ src/core/validation.rs
@@ -5,10 +5,10 @@ use num_traits::Float;
 pub fn validation_shape_2d_1d<T, S1, S2>(x: &ArrayBase<S1, Dim<[usize; 2]>>, y: &ArrayBase<S2, Dim<[usize; 1]>>) -> Result<(), DataError>
 where
     T: Float,
-    S1: ndarray::Data<Elem = T>, // T doit être le type des éléments
+    S1: ndarray::Data<Elem = T>,
     S2: ndarray::Data<Elem = T>,
 {
-    // --- 1. Vérification des données vides ---
+    // --- 1. Verification of empty data ---
     if x.is_empty() {
         return Err(DataError::EmptyData);
     }
@@ -17,18 +17,17 @@ where
         return Err(DataError::EmptyTarget);
     }
 
-    // --- 2. Vérification des NaN (Not a Number) ---
-    // Utilise le trait Float pour appeler .is_nan() sur chaque élément de X.
+    // --- 2. Verification of NaN (Not a Number) ---
     if x.iter().any(|&e| e.is_nan()) {
         return Err(DataError::NaNData);
     }
 
-    // --- 3. Vérification de la correspondance des dimensions (Échantillons) ---
-    let n_samples_x = x.shape()[0]; // Nombre de lignes dans X
-    let n_samples_y = y.shape()[0]; // Nombre d'éléments dans Y
+    // --- 3. Verification of dimensional correspondence (Samples) ---
+    let n_samples_x = x.shape()[0];
+    let n_samples_y = y.shape()[0];
 
     if n_samples_x != n_samples_y {
-        // Retourne une erreur DimensionMismatch indiquant les tailles trouvées.
+        // Returns a DimensionMismatch error indicating the sizes found.
         return Err(DataError::DimensionMismatch(n_samples_x, n_samples_y));
     }
 
